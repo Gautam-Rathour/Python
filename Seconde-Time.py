2c694badd7c9a13110df7a849ae647805586b1d8
Gautam-Rathour <gau321tam@gmail.com>
2026-08-16 10:44:59 +0530

    Python practic Code

--- Seconde-Time.py
+++ Seconde-Time.py
@@ -8,7 +8,71 @@
 
 # print(8 ** 3);
 
-print('Ram ' * 10);
+# print('Ram ' * 10);
+
+# name = "Mohit";
+# # print(name + " how are you ?");
+# # print(name[-3]);
+# print(name[2:5]);
+
+# name = "my name is Raju \nand i am a programmer";
+# print(name); 
+
+
+
+
+# // =====================================================================
+# // =====================================================================
+
+
+# # List
+
+# nums = [34, 56, 83, 29, 98];
+# # print(nums);
+# # print(nums[-1]);
+
+# names = ['navin', 'kiran', 'harsh', 'gaut'];
+# # print(names);
+# # print(names[0]);
+
+# mix = [34, 56, 83, 29, 98, 'gaut'];
+# # print(mix);
+
+# mix = [[34, 56, 83, 29, 98], ['navin', 'kiran', 'harsh', 'gaut']];
+# print(mix[0][2]);
+
+# nums.append(290);
+# nums.count(29);
+# nums.insert(1, 60);
+# nums.remove(56);
+# nums.pop();
+# nums.reverse();
+# nums.sort();
+# print(nums);
+
+# // =====================================================================
+# // =====================================================================
+
+
+# Tuple ( can't change)
+
+# tup = 23, 45, 54, 28, 98;
+
+# tup[2] = 90;
+
+# print(tup);
+# print(tup[2]);
+
+tupA = (2, 'navin', 7.2, [3, 6, 9, 2]);
+tupA[3][1] = 7;
+print(tupA);
+
+
+
+
+
+
+
 
 
 
